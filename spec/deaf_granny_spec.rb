02b3f0909e7b69e265d5175f2d38b59ddedd4dce
KOTP <keeperotphones@gmail.com>
2023-02-14 11:15:26 -0500

    Update for minitest object testing

--- spec/deaf_granny_spec.rb
+++ spec/deaf_granny_spec.rb
@@ -5,59 +5,59 @@ describe 'Granny' do
   before :each do
     @granny = Granny.new
   end
-  
+
   it 'must reply to sonny when nothing is said' do
-    @granny.replies_to('').must_match 'sit'
+    _(@granny.replies_to('')).must_match 'sit'
   end
   it 'must reply to sonny when BYE is said' do
-    @granny.replies_to('BYE').must_match 'flies'
+    _(@granny.replies_to('BYE')).must_match 'flies'
   end
   it 'must reply with some year when ALLCAPS' do
-    @granny.replies_to('HELLO').must_match "NOT SINCE "
+    _(@granny.replies_to('HELLO')).must_match "NOT SINCE "
   end
   it 'must reply with HUH when not yelling' do
-    @granny.replies_to('hello').must_match 'HUH'
+    _(@granny.replies_to('hello')).must_match 'HUH'
   end
 end
 
 
 describe 'Integration Testing' do
   include Capybara::DSL
-  before :each do    
+  before :each do
     visit '/'
   end
   it 'must have title of Deaf Granny' do
-    page.must_have_content 'Deaf'
+    _(page).must_have_content 'Deaf'
   end
   it 'must return appropriate page for silence' do
       fill_in 'sonny_says', :with => ''
       click_button 'response'
-      page.must_have_content "Don't sit there"
+      _(page).must_have_content "Don't sit there"
   end
 
   it 'must return appropriate page for BYE' do
       fill_in 'sonny_says', :with => 'BYE'
       click_button 'response'
-      page.must_have_content "Don't sit there"
+      _(page).must_have_content "Don't sit there"
   end
 
   it 'must return appropriate page for HELLO' do
       fill_in 'sonny_says', :with => 'HELLO'
       click_button 'response'
-      page.must_have_content "NOT SINCE"
+      _(page).must_have_content "NOT SINCE"
   end
 
   it 'must have stop message for three BYE consecutively' do
       fill_in 'sonny_says', :with => 'BYE'
       click_button 'response'
-      page.must_have_content "sit there"
+      _(page).must_have_content "sit there"
       find_link('Click Here').click
       fill_in 'sonny_says', :with => 'BYE'
       click_button 'response'
-      page.must_have_content "sit there"
+      _(page).must_have_content "sit there"
       find_link('Click Here').click
       fill_in 'sonny_says', :with => 'BYE'
       click_button 'response'
-      page.must_have_content "BYE SONNY"
+      _(page).must_have_content "BYE SONNY"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $:.unshift('./lib', './spec')
 require 'minitest/autorun'
-require 'minitest/reporters' 
+require 'minitest/reporters'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara_minitest_spec'
@@ -11,14 +11,14 @@ set :environment, :test
 
 # To select a different reporter copy the spec/spec_config.rb.example to
 # spec/spec_config.rb and select the reporter you wish to use.
-File.exists?('spec/spec_config.rb') ?
+File.exist?('spec/spec_config.rb') ?
   require( 'spec_config') :
   Option = {report_style: "Default"}
 MiniTest::Reporters.use!(eval("MiniTest::Reporters::#{Option[:report_style]}Reporter.new"))
 
 class MiniTest::Spec
   include Capybara::DSL
-  Capybara.app = Sinatra::Application.new 
+  Capybara.app = Sinatra::Application.new
 end
 
 
